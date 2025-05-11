44abe2e22ad718eeaf77d1f976067779124814f2
dursunalisevinc <sevincdursunali61@gmail.com>
2025-05-11 15:52:18 +0300

    front git eklendi

---
+++ front/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './shared/Layout';
+import Dashboard from './pages/Dashboard';
+import ProductPage from './pages/Product';
+import LoginPage from './pages/Auth/Login';
+import RegisterPage from './pages/Auth/Register';
+import PrivateRoute from './component/PrivateRoute';
+import AdminDashboard from './AdminPages/Dashboard';
+import Unauthorized from './pages/Unauthorized';
+
+function App() {
+  return (
+  <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="products" element={<ProductPage />} />
+          </Route>
+        </Route>
+
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ front/src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+const Index = () => {
+
+
+    return (
+        <div>
+            user ürün sayfası burası
+        </div>
+    );
+};
+
+export default Index;
