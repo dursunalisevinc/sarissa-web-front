bcbea9efc0e53a084eb7dbde16cafb33c487db11
Mehmet Ali <sevincmehmetali716@gmail.com>
2025-05-19 13:14:32 +0300

    confilic

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')).render(
     <App />
